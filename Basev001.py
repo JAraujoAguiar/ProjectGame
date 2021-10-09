cabe08a78724da348e8929e4c3af07905467feae
JAraujoAguiar <87349284+JAraujoAguiar@users.noreply.github.com>
2021-10-09 16:15:58 -0700

    Add files via upload

--- Basev001.py
+++ Basev001.py
@@ -1,9 +1,32 @@
 import time
-import turtle
+#import tkFont
+import pandas as pd
+from tkinter import *
+
+
+window = Tk()
+window.title("Tomb Stuck")
+window.configure(background="black")
+#font = tkFont.Font ()
+Label (window, bg="black").grid(row=10, column=5)
 
 #-------------------------Classes----------------------#
+class Leaderboard: #Handles the input to 
+    name = ['Jesus A']
+    ending = [True]
+    score = ['30'] 
+
+    def Stamp(name, ending, score):
+        Leaderboard.name.append(name)
+        Leaderboard.ending.append(ending)
+        Leaderboard.score.append(score)
+        leaderboard = pd.DataFrame({'Name': Leaderboard.name, 'Ending': Leaderboard.ending, 'Score': Leaderboard.score})
+        #leaderboard = leaderboard.sort([score], ascending=[1])
+        print(f'Leaderboard: \n \n {leaderboard}')
+
+
 class Inventory: #Handles inventory management
-    inventory = ["space", "item"]
+    inventory = [None]
     
     def AddInventory(self, item):
         Inventory.inventory.append(item)
@@ -18,7 +41,7 @@ class Timer: #Handles matters of time
         if Timer.time < 0:
             Timer.time = 0
             Timer.Passing
-            print(f"\nYou have {Timer.time} minutes left\n")
+            Label(window, text=f"\nYou have {Timer.time} minutes left\n")
             Timer.Passing
             return(Timer.time) 
 
@@ -33,113 +56,161 @@ class Timer: #Handles matters of time
             print(f"\nYou have {Timer.time} minutes left\n")
             Timer.Passing
             return(Timer.time) 
-               
 
     def Passing():
-        time.sleep(2)
+        time.sleep(3)
 
 class Game: #Handles the game interactions and responses
+    name = None
+    ending = True
+    score = Timer.time
+    answer = None
+    row = None
+    
+    def Print(phrase):
+        Timer.Passing()
+        Label (window, text=f'\n{phrase}\n\n', bg='black', fg="white", font="none 12 bold").grid(row=Game.row, column=0, sticky=W)
+        Game.row = Game.row + 1
+        return(Game.row)
+
+    def Question(question):
+        Game.answer = None
+        Timer.Passing()
+        Game.answer = input(f'\n{question}\n').lower().strip()
+        return(Game.answer)
 
     def Over():
-        print("\nYour mind drifts away as the current washes your body away... \n")
         Timer.Ticking(Timer.time, 30)
-        print("GAME OVER")
+        Game.Print("Your mind drifts away as the current washes your body away...")
+        Game.Print("GAME OVER")
+        Game.ending = False
+        Leaderboard.Stamp(Game.name, Game.ending, Game.score)
     
     def BadAnswer():
         print("\nYou can't seem to think straight... \n")
         seed = Game.Over()
 
     def Drown():
-        Timer.Passing()
-        print("\nWater has filled out the room\n")
-        Timer.Passing()
-        print("\nIn a futile effort to survive you cling to the last air pocket in the room\n")
-        Timer.Passing()
-        print("\nIt is quickly filled out with water and so are your lungs\n")
-        Timer.Passing()
+        Game.Print("Water has filled out the room")
+        Game.Print("In a futile effort to survive you cling to the last air pocket in the room")
+        Game.Print("It is quickly filled out with water and so are your lungs")
         seed = Game.BadAnswer()
 
+    def Win():
+        Game.Print("You crawl out of the hole you made in the wall...")
+        Game.Print("Although you still don't know how you got there...")
+        Game.Print("However, soon after reaching the surface...")
+        Game.Print("You realize why you were there.")
+        Game.Print("TO BE CONTINUED")
+        Leaderboard.Stamp(Game.name, Game.ending, Game.score)
+
+
+    def TimerCheck():
+        if Timer.time <= 0:
+            Game.Drown()
+        else:
+            pass
 
     def Start():
-        print(f"\nSlowly you come back to. Your head is throbbing, your body aches and your eyes are heavy...\n")
-        Timer.Passing()
-        answer = input("Would you open your eyes? (yes/no) \n \n").lower().strip()
-        if answer == "yes":
-            game.Q1()
-        elif answer == "no":
-            game.Over()
+        Game.Print(f"Slowly you come back to. Your head is throbbing, your body aches and your eyes are heavy...")
+        Game.Question("Would you open your eyes? (yes/no)")
+        if Game.answer == "yes":
+            Game.Q1()
+        elif Game.answer == "no":
+            Game.Over()
         else:
-            game.BadAnswer()
+            Game.BadAnswer()
         
     def Q1():
         Timer.Ticking(Timer.time, 0)
-        print("\nIt's dark, however a small crack on the ceiling gives just enough light to make your sorroundings... \n")
+        Game.Print("It's dark, however a small crack on the ceiling gives just enough light to make your sorroundings...")
+        Game.Print("You are able to hear water rushing in from somewhere you cannot see...")
         Timer.Passing()
-        print("\nYou are able to hear water rushing in from somewhere you cannot see...\n")
-        Timer.Passing()
-        answer = input("\n What would you like to do? (Look around/Close eyes)\n\n").lower().strip()
+        Game.Question("What would you like to do? (Look around/Close eyes)")
 
-        if answer == "look around":
-            game.Q2a()
+        if Game.answer == "look around":
+            Game.Q2a()
 
-        elif answer == "close eyes":
-            game.Over()
+        elif Game.answer == "close eyes":
+            Game.Over()
 
         else:
-            game.BadAnswer()
+            Game.BadAnswer()
     
     def Q2a():
+        Game.TimerCheck()
         Timer.Ticking(Timer.time, 1)
-        print("\nYou can make out in the darkness a section of the wall that seems to have some loose rocks...\n")
-        Timer.Passing()
-        answer = input("What would you do? (Get closer/Keep looking)\n").lower().strip()
+        Game.Print("You can make out in the darkness a section of the wall that seems to have some loose rocks...")
+        Game.Question("What would you do? (Get closer/Keep looking)")
 
-        if answer == "get closer":
-            answer = None
-            game.Q3
+        if Game.answer == "get closer":
+            Game.Q3()
 
-        elif answer == "keep looking":
-            answer = None
+        elif Game.answer == "keep looking":
             for item in Inventory.inventory:
                 if item == 'Macuahuitl':
-                    print("\nYou don't see anything else that might be relevant")
-                    game.Q2a()
-            
+                    Game.Print("You don't see anything else that might be relevant")
+                    Game.Q2a()
+    
                 else:
-                    Timer.Passing()
-                    game.Q2b()
+                    Game.Q2b()
+                
+                break
 
         else:
-            game.BadAnswer()
+            Game.BadAnswer()
 
     def Q2b():
-        Timer.Passing()
-        print("You see a Macuahuitl lying on the floor...")
-        Timer.Passing()
-        answer = input("What would you do? (Take/Go back)").lower().strip()
+        Game.TimerCheck()
+        Game.Print("You see a Macuahuitl lying on the floor...")
+        Game.Question("What would you do? (Take/Go back)")
 
-        if answer == "take":
-            answer = None
-            Inventory.AddInventory(Inventory.inventory, "Macuahuitl")
-            game.Q2a()
+        if Game.answer == "take":
+            Inventory.AddInventory(Inventory.inventory, 'Macuahuitl')
+            Game.Q2a()
 
-        elif answer == "go back":
-            answer = None
-            game.Q2a()
+        elif Game.answer == "go back":
+            Game.Q2a()
         
-        elif answer == "what is a macuahuitl?":
-            print("A macuahuitl is a weapon, a wooden club with several embedded obsidian blades. The name is derived from the Nahuatl language and means hand-wood. Its sides are embedded with prismatic blades traditionally made from obsidian. Obsidian is capable of producing an edge sharper than high quality steel razor blades. The macuahuitl was a standard close combat weapon.")
+        elif Game.answer == "what is a macuahuitl?":
+            Game.Print("A macuahuitl is a weapon, a wooden club with several embedded obsidian blades. The name is derived from the Nahuatl language and means hand-wood. Its sides are embedded with prismatic blades traditionally made from obsidian. Obsidian is capable of producing an edge sharper than high quality steel razor blades. The macuahuitl was a standard close combat weapon.")
             Timer.Ticking(Timer.time, 30)
 
         else:
-            game.BadAnswer()
+            Game.BadAnswer()
         
     def Q3():
-        print("To be continued...")
+        Game.TimerCheck()
+        Game.Print('You see that water trickles through this wall...')
+        Game.Print('You think that if you hit the wall hard enough you can bring it down...')
+        Game.Question('What would you do? (Hulk Smash/Keep Looking)')
+        if Game.answer == "hulk smash":
+            for item in Inventory.inventory:
+                if item == 'Macuahuitl':
+                    Game.Print("You use the Macuahuitl to smash the wall in with all your might...")
+                    Game.Print("The wall gives away, revealing a passage to the outside...")
+                    Game.Q2a()
+                else:
+                    Timer.Ticking(Timer.Ticking, 10)
+                    Game.Print("You try for a good while to hit the wall but it seems that your bare hands can't do much")
+                    Game.Q3()
+                break
+
+        elif Game.answer == "keep looking":
+            for item in Inventory.inventory:
+                if item == 'Macuahuitl':
+                    Game.Print("You don't see anything else that might be relevant")
+                    Game.Q3()
+
+                else:
+                    Game.Q2b()
+                
+                break
+
         
 #-------------------------GameInit----------------------#
 
 game = Game
+game.name = game.Question('What is your name?')
 game.Start()
-
-
+window.mainloop()
